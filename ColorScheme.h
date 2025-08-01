5ccfe4b8bc20e1fa28287d858eebf9aca7441c0b
Starkku <starkku.ppmod@gmail.com>
2025-08-02 02:05:19 +0300

    Update ColorScheme & MessageListClass
    - Deprecate superfluous/potentially inaccurate hardcoded color scheme index enum and update PrintMessage to have non-hardcoded default color

--- ColorScheme.h
+++ ColorScheme.h
@@ -15,17 +15,19 @@ class LightConvertClass;
 class ColorScheme
 {
 public:
-	enum {
+
+	enum [[deprecated("Only valid for vanilla color scheme configuration and should not be used.")]]
+	{
 		//ColorScheme indices, since they are hardcoded all over the exe, why shan't we do it as well?
-		Yellow  =   3,
-		White   =   5,
-		Grey    =   7,
-		Red     =  11,
-		Orange  =  13,
-		Pink    =  15,
-		Purple  =  17,
-		Blue    =  21,
-		Green   =  29,
+		Yellow = 3,
+		White = 5,
+		Grey = 7,
+		Red = 11,
+		Orange = 13,
+		Pink = 15,
+		Purple = 17,
+		Blue = 21,
+		Green = 29,
 	};
 
 	//global array
